5ff7258f7808469ae81ee44bc436e6379fe11e9b
Nataliia Chala <hatalochka.rv@gmail.com>
2022-07-12 21:06:03 +0100

    added link to github

--- src/App.js
+++ src/App.js
@@ -4,5 +4,5 @@ import WeatherSearch from "./WeatherSearch";
 export default function App() {
     return <div className = 'App'> <h1> Weather Search Engine </h1> 
     <WeatherSearch/> 
-    <p><a href="">Open-source code </a>by Nataliia Chala</p></div> ;
+    <p><a href="https://github.com/natalie-0073/search-engine.git" target="_blank">Open-source code </a>by Nataliia Chala</p></div> ;
 }
